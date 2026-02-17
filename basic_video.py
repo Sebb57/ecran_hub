2966e1483051d030fa54ce16eefa6e66f0531e8e
Sébastien Doussang <sebastien.doussang@epitech.eu>
2026-02-17 21:33:52 +0100

    fix: lighter video on startup

--- basic_video.py
+++ basic_video.py
@@ -11,7 +11,7 @@ import time
 
 HOST = "127.0.0.1"
 PORT = 6767
-DEFAULT_VIDEO = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+DEFAULT_VIDEO = "https://www.youtube.com/shorts/lIayj4dtmcI"
 
 def send_video(url):
     try:
