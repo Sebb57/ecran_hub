4bc49666c838a485fe06c38e9a252a6a6569be52
Sébastien Doussang <sebastien.doussang@epitech.eu>
2026-02-17 21:35:32 +0100

    fix: proper way to source python env

--- starter.sh
+++ starter.sh
@@ -3,11 +3,17 @@
 cd ecran_hub || exit
 git pull
 
-source my_venv/bin/activate
+if [ -f "./my_venv/bin/activate" ]; then
+    source ./my_venv/bin/activate
+else
+    echo "Error: my_venv not found in $(pwd)"
+    exit 1
+fi
 streamlit run dashboard.py &
 
 ./ecran_hub 6767 . &
-sleep 2
+
+sleep 5
 
 python3 basic_video.py
 
